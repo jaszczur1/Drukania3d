ff4982947f8ce89b9204bb6ae3047aabe1fc9f42
jaszczur1 <m1jadczak@gmail.com>
2017-09-06 00:47:18 +0200

    add dialog jquery

--- public_html/javascripts/mainjs.js
+++ public_html/javascripts/mainjs.js
@@ -4,31 +4,76 @@
  * and open the template in the editor.
  */
 
- $(document).ready(function (){
-     
-              $("#home-nav").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("#home-nav").offset().top +100
-                }, 2000);
-            });
-            
-     
-            $("#portfolio-nav").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("#portfolio-div").offset().top -100
-                }, 2000);
-            });
-            
-            $("#about-nav").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("#about-div").offset().top
-                }, 1000);
-            });
-            
-             $("#contact-nav").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("#contact-div").offset().top
-                }, 800);
-            });
-            
+$(document).ready(function () {
+
+    $("#home-nav").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#home-nav").offset().top + 100
+        }, 2000);
+    });
+
+
+    $("#portfolio-nav").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#portfolio-div").offset().top - 100
+        }, 2000);
+    });
+
+    $("#about-nav").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#about-div").offset().top
+        }, 1000);
+    });
+
+    $("#contact-nav").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#contact-div").offset().top
+        }, 800);
+    });
+
+    var email = [];
+    var check_point;
+    var postion_dot;
+    $("#inputHorizontalSuccess").keypress(function (e) {
+//        console.log(String.fromCharCode(e.keyCode));
+        email.push(String.fromCharCode(e.keyCode));
+
+        for (var i = 0; i < email.length; i++) {
+            console.log(email);
+            if (email[i] == '@' && i > 0)
+                check_point = true; // bool position @            
+            if (check_point == true && email[i] == '.') {
+                postion_dot = i;
+            }
+            if (postion_dot < i)
+                $('#add-class-for-email').addClass('has-success');
+            //    $('#add-class-for-email').removeClass('.has-success');
+
+
+
+        }
+
+
+
+    });
+    $('#v').click(function () {
+        $("#dialog").dialog({
+            height: 400,
+            width : 500,
+            dialogClass: "no-close",
+            buttons: [
+                {
+                    text: "OK",
+                    click: function () {
+                        $(this).dialog("close");
+                    }
+                }
+            ]
         });
+    });
+});
+
+
+
+
+        
